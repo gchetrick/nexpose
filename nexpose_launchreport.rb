6587f0a36044f96b03ce2287209b0cd95936b18c
root <root@localhost.localdomain>
2015-09-17 18:16:22 -0500

    Added ability to kick off a report when a site is not running

--- nexpose_launchreport.rb
+++ nexpose_launchreport.rb
@@ -3,15 +3,13 @@ require 'nexpose'
 require 'optparse'
 include Nexpose
 
-##Purpose of this is to run a rescan of created asset group
-##gch - 9/9/2015
+##Purpose is to see if a site currently has a scan running, if it does not then kick off a report.
+##gch - 9/17/2015
 
 #Set these to match your environment
-nexpose_host ''
+nexpose_host = ''
 username = ''
 password = ''
-
-options = {:agroup => nil}
 
 parser = OptionParser.new do |opts|
 opts.banner = "Usage: nexpose_scan.rb [options]"
@@ -19,16 +17,29 @@ opts.banner = "Usage: nexpose_scan.rb [options]"
 opts.on("-i", "--id siteid",OptionParser::DecimalInteger, "Site ID") do |i|
   options[:id] = i;
   end
+opts.on("-r", "--rid reportid",OptionParser::DecimalInteger, "Report ID") do |r|
+  options[:rid] = r;
+  end
 end
 
 parser.parse!
 
+#create the connection and login
 nsc = Connection.new(nexpose_host, username , password)
 nsc.login
+#grab scan activity
 runningScans = nsc.scan_activity()
 runningScans.each do |status|
   if options[:id] == status.site_id
     puts " Site ID: #{status.site_id}  Status #{status.status}"
+  else
+    reports = nsc.list_reports
+    #uncomment to list all the reports with name and ID
+    #reports.each do |report|
+    #  puts "#{report.name} - #{report.config_id}"
+    #end
+    nsc.generate_report(options[:rid])
+    puts "#{report.name} - #{report.config_id} has been started"
   end
 end
 nsc.logout
